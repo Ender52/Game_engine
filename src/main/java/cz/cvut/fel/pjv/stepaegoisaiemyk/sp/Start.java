857ae94969d6b2edfb02ae33ed3f7376200228a9
stepaego <stepaego@fel.cvut.cz>
2017-05-21 02:30:15 +0200

    Added basic logger, should be added everywhere, but not yet

--- src/main/java/cz/cvut/fel/pjv/stepaegoisaiemyk/sp/Start.java
+++ src/main/java/cz/cvut/fel/pjv/stepaegoisaiemyk/sp/Start.java
@@ -1,8 +1,20 @@
 package cz.cvut.fel.pjv.stepaegoisaiemyk.sp;
 
 
+import cz.cvut.fel.pjv.stepaegoisaiemyk.sp.utils.Log;
+
+import java.util.logging.Level;
+
 public class Start{
+
     public static void main(String[] args) {
         Game game = new Game();
+        try {
+            Log my_log = new Log("log.txt");
+            my_log.logger.setLevel(Level.ALL);
+            my_log.logger.info("Info msg");
+            my_log.logger.severe("Sever msg");
+            //log.info("Game started");
+        } catch (Exception e){}
     }
 }
